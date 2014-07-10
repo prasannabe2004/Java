203356a0ae76c533e48c796b780c4bf76e8ddde9
prasannabe2004 <prasannabe2004@gmail.com>
2014-07-10 18:45:33 -0500

    Adding examples for java

---
+++ udacity/lesson1/Errors.java
@@ -0,0 +1,7 @@
+public class Errors
+{
+	public static void main(String[] args)
+	{
+		System.out.print(1/0);
+	}
+}
---
+++ udacity/lesson1/TextNumbers.java
@@ -0,0 +1,8 @@
+public class TextNumbers
+{
+	public static void main(String[] args)
+	{
+		System.out.print("3 + 4 + 5");
+		System.out.println(3 + 4 + 5);
+	}
+}
