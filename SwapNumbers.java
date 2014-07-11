bbf6f6d4eac8741d647fd0998625296d322b0da3
Prasanna <prasanna@localhost.localdomain>
2014-07-11 09:48:28 -0500

    Swap program without using temp variable using +,-,^ operators.

---
+++ SwapNumbers.java
@@ -0,0 +1,28 @@
+class SwapNumbers
+{
+
+	public static void main(String[] args) 
+	{
+		int a = 10;
+		int b = 20;
+
+		System.out.println("Orginal Numbers are " + a + " and " + b);
+
+		System.out.println("Lets try with + and - operators first");
+		a = a + b;
+		b = a - b;
+		a = a - b;
+
+		System.out.println("Swapped Numbers are " + a + " and " + b);
+
+		a = 10;
+		b = 20;
+
+		System.out.println("Lets try with bit operators");
+		a = a ^ b;
+		b = a ^ b;
+		a = a ^ b;
+		System.out.println("Swapped Numbers are " + a + " and " + b);
+
+	}
+}
