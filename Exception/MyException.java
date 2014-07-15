e1da041672a155accc7aa99fa88b8a93d81f45da
prasannabe2004 <prasannabe2004@gmail.com>
2014-07-15 10:37:55 -0500

    Arthemetic exception.

---
+++ Exception/MyException.java
@@ -0,0 +1,28 @@
+
+
+ 
+public class MyException 
+{
+    public static int GenerateRuntimeEx()
+    {
+        try
+        {
+            for(int i=5;i>=0;i--)
+            {
+                System.out.println(10/i);
+            }
+        }
+        catch (ArithmeticException e)
+        {
+            System.out.println("Exception... " + e.getMessage());
+            e.printStackTrace();
+        }
+        System.out.println("After for loop... ");
+        return 1;
+    }
+
+    public static void main(String a[])
+    {
+        GenerateRuntimeEx();
+    }
+}
