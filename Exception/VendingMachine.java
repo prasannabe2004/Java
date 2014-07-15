931270dbe3e3d58dd978a193b5d9a118c002f214
prasannabe2004 <prasannabe2004@gmail.com>
2014-07-15 08:30:20 -0500

    Handling individual Exception for cold and hot temperature.

--- Exception/VendingMachine.java
+++ Exception/VendingMachine.java
@@ -1,30 +1,50 @@
 
 class TemperatureException extends Exception 
 {
+    private int temperature; // in Celsius
+    
+    public TemperatureException(int temperature) 
+    {
+        this.temperature = temperature;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
 }
 
 class TooColdException extends TemperatureException 
 {
+    public TooColdException(int temperature) 
+    {
+        super(temperature);
+    }
 }
 
 class TooHotException extends TemperatureException 
 {
+    public TooHotException(int temperature) 
+    {
+        super(temperature);
+    }
 }
 
 
-class VirtualPerson {
+class VirtualPerson 
+{
     private static final int tooCold = 65;
     private static final int tooHot = 85;
+
     public void drinkCoffee(CoffeeCup cup) throws TooColdException, TooHotException 
     {
         int temperature = cup.getTemperature();
         if (temperature <= tooCold) 
         {
-            throw new TooColdException();
+            throw new TooColdException(temperature);
         }
         else if (temperature >= tooHot) 
         {
-            throw new TooHotException();
+            throw new TooHotException(temperature);
         }
         //...
     }
@@ -55,22 +75,56 @@ class VirtualCafe
             cust.drinkCoffee(cup);
             System.out.println("Coffee is just right.");
         }
+
+
         catch (TooColdException e) 
         {
-            System.out.println("Coffee is too cold.");
-            // Deal with an irate customer...
+            int temperature = e.getTemperature();
+            System.out.println("Coffee temperature is " + temperature + " degrees Celsius.");
+            if (temperature > 55 && temperature <= 65) 
+            {
+                System.out.println("Coffee is cooling off.");
+                // Add more hot coffee...
+            }
+            else if (temperature > 0 && temperature <= 55) 
+            {
+                System.out.println("Coffee is too cold.");
+                // Give customer a new cup of coffee with the
+                // proper temperature...
+            }
+            else if (temperature <= 0) 
+            {
+                System.out.println("Coffee is frozen.");
+                // Deal with an irate customer...
+            }
         }
         catch (TooHotException e) 
         {
-            System.out.println("Coffee is too hot.");
-            // Deal with an irate customer...
+           int temperature = e.getTemperature();
+            System.out.println("Coffee temperature is " + temperature + " degrees Celsius.");
+            if (temperature >= 85 && temperature < 100) 
+            {
+                System.out.println("Coffee is too hot.");
+                // Ask customer to let it cool a few minutes...
+            }
+            else if (temperature >= 100 && temperature < 2000) 
+            {
+                System.out.println("Both coffee and customer are steamed.");
+                // Deal with an irate customer...
+            }
+            else if (temperature >= 2000) 
+            {
+                System.out.println("The coffee is plasma.");
+                // Deal with a very irate customer...
+            }
         }
     }
 }
 
+
 class VendingMachine 
 {
-    public static void main(String[] args) 
+    public static void main(String[] args)
     {
         int temperature = 0;
         if (args.length > 0) 
