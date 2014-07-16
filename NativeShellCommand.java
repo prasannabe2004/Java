461ed69260e0426e0078f9981be313415bb7c649
prasannabe2004 <prasannabe2004@gmail.com>
2014-07-16 16:43:08 -0500

    To run shell commands from java.

---
+++ NativeShellCommand.java
@@ -0,0 +1,36 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class NativeShellCommand
+{
+	public static StringBuffer ExcecuteShellCommand(String cmd)
+	{
+		int pid = -1;
+		StringBuffer output = new StringBuffer();
+		try 
+		{
+    		Process process = Runtime.getRuntime().exec(cmd);
+    		process.waitFor();
+    		
+    		BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+    		String line = "";			
+			while ((line = reader.readLine())!= null) 
+			{
+				output.append(line + "\n");
+			}
+		}
+		catch (Exception e) 
+		{
+    		e.printStackTrace(System.err);
+    	}
+    	return output;
+    }
+
+	public static void main (String args[])
+	{
+		StringBuffer out = ExcecuteShellCommand("ping google.com");
+		System.out.println(out);
+    }
+}
+
