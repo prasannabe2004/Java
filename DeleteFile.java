8196018d5f68781bf650ac20d034630cf1b6efbd
prasannabe2004 <prasannabe2004@gmail.com>
2014-07-17 09:24:39 -0500

    Delete a file.

---
+++ DeleteFile.java
@@ -0,0 +1,36 @@
+import java.io.*;
+
+public class DeleteFile
+{
+    public static void DeleteMyFile(String filename)    
+    {
+        try
+        {
+
+            File file = new File("filename");
+            try
+            {
+                if(file.exists())
+                {
+                    if(file.delete())
+                    {
+                        System.out.println(file.getName() + " is deleted!");
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args)
+    {	
+        DeleteMyFile("/home/prasanna/test.txt");    
+    }
+}
