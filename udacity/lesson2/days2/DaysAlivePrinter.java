f7c7b6552b90cde4dfe52e981e0f4eab9c9365e3
prasannabe2004 <prasannabe2004@gmail.com>
2014-08-05 18:49:09 -0500

    Lesson 2 examples tested.

--- udacity/lesson2/days2/DaysAlivePrinter.java
+++ udacity/lesson2/days2/DaysAlivePrinter.java
@@ -1,12 +1,24 @@
+// Calculate the number of days live
 public class DaysAlivePrinter
 {
     public static void main(String[] args)
     {
+        //Get a new instance for birthday
         Day birthDay = new Day(1951, 5, 26);
+        
+        //Get a new instance for lastday
         Day lastDay = new Day(2012, 7, 23);
+        
+        //Lets print the birthday
+        System.out.print("BirthDay: ");
+        System.out.println(birthDay.toString());
+
+        //Lets print the lastday
         System.out.print("LastDay: ");
         System.out.println(lastDay.toString());
+        
         int daysAlive = lastDay.daysFrom(birthDay);
+        
         System.out.print("Days alive: ");
         System.out.println(daysAlive);
     }
--- udacity/lesson2/rectangles/RectangleDemo.java
+++ udacity/lesson2/rectangles/RectangleDemo.java
@@ -2,8 +2,10 @@ public class RectangleDemo
 {
     public static void main(String[] args)
     {
-        Rectangle box = new Rectangle(5, 10, 60, 90);
+        Rectangle box = new Rectangle(60, 90, 20, 30);
+        Rectangle box2 = new Rectangle(80, 120, 20, 30);
         box.draw();
-        box.translate(200, 100);
+        box2.draw();
     }
 }
+
