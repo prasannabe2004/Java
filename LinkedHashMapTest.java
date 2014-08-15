bbfdfe8b6da64addbf12516e35a14d464e6bec62
prasannabe2004 <prasannabe2004@gmail.com>
2014-08-15 15:13:20 -0500

    compilation errors fixed.

--- LinkedHashMapTest.java
+++ LinkedHashMapTest.java
@@ -4,7 +4,7 @@ public class LinkedHashMapTest {
 
    public static void main(String args[]) {
       // Create a hash map
-      LinkedHashMap lhm = new LinkedHashMap();
+      LinkedHashMap<String, Double> lhm = new LinkedHashMap<String, Double>();
       // Put elements to the map
       lhm.put("Zara", new Double(3434.34));
       lhm.put("Mahnaz", new Double(123.22));
@@ -13,9 +13,9 @@ public class LinkedHashMapTest {
       lhm.put("Qadir", new Double(-19.08));
       
       // Get a set of the entries
-      Set set = lhm.entrySet();
+      Set<?> set = lhm.entrySet();
       // Get an iterator
-      Iterator i = set.iterator();
+      Iterator<?> i = set.iterator();
       // Display elements
       while(i.hasNext()) {
          Map.Entry me = (Map.Entry)i.next();
