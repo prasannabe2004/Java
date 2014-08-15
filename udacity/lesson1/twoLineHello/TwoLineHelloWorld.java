c344e31eb6c0173bc8eeb398f8c91330ec934a5a
prasannabe2004 <prasannabe2004@gmail.com>
2014-08-15 17:22:01 -0500

    Test program for hello world completed.

--- udacity/lesson1/twoLineHello/TwoLineHelloWorld.java
+++ udacity/lesson1/twoLineHello/TwoLineHelloWorld.java
@@ -1,3 +1,5 @@
+package udacity.lesson1.twoLineHello;
+
 // Look for lines that begin with two slashes! These are "comments" and 
 // we will use them to let you know what you should do on each programming
 // exercise. Have fun!
@@ -7,7 +9,8 @@ public class TwoLineHelloWorld
     public static void main(String args[])
     {
         // Write your code to print "Hello" and "World" on separate lines below
-        // Hint: You can use System.out.println("your text here");
+        System.out.println("Hello");
+    	System.out.println("World");
         // Hint2: You want to use System.out.println twice. Once to print "Hello" and a second time to print "World" 
     }
 }
