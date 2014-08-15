d39885a7d7d3fbbae201b4a0d7b9eea10847b9b1
prasannabe2004 <prasannabe2004@gmail.com>
2014-08-15 18:56:43 -0500

    Fixed compilation issue.

--- udacity/lesson2/rectangles/Rectangle.java
+++ udacity/lesson2/rectangles/Rectangle.java
@@ -1,3 +1,5 @@
+package udacity.lesson2.rectangles;
+
 //HIDE
 import java.awt.Graphics2D;
 import java.awt.geom.Rectangle2D;
--- udacity/lesson2/rectangles/RectangleDemo.java
+++ udacity/lesson2/rectangles/RectangleDemo.java
@@ -1,11 +1,15 @@
+package udacity.lesson2.rectangles;
+
 public class RectangleDemo
 {
     public static void main(String[] args)
     {
-        Rectangle box = new Rectangle(60, 90, 20, 30);
-        Rectangle box2 = new Rectangle(80, 120, 20, 30);
-        box.draw();
-        box2.draw();
+        //Rectangle box = new Rectangle(60, 90, 20, 30);
+        //Rectangle box2 = new Rectangle(80, 120, 20, 30);
+        //box.draw();
+        //box2.draw();
+        Rectangle box3 = new Rectangle(0,50,20,20);
+        box3.draw();
     }
 }
 
