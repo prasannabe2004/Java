1c2dab9f2138f28c8d15a5ac95c7c348a9c848ce
prasannabe2004 <prasannabe2004@gmail.com>
2014-08-16 16:36:50 -0500

    re-org

---
+++ udacity/lesson3/cars4/Car.java
@@ -0,0 +1,37 @@
+package udacity.lesson3.lesson3.cars4;
+
+public class Car
+{
+    private double milesDriven;
+    private double gasInTank;
+    private double milesPerGallon;
+
+    public Car()
+    {
+        milesDriven = 0;
+        gasInTank = 0;
+        milesPerGallon = 50;
+    }
+
+    public void drive(double distance)
+    {
+        milesDriven = milesDriven + distance;
+        double gasConsumed = distance / milesPerGallon;
+        gasInTank = gasInTank - gasConsumed;
+    }
+
+    public void addGas(double amount)
+    {
+        gasInTank = gasInTank + amount;
+    }
+
+    public double getMilesDriven()
+    {
+        return milesDriven;
+    }
+
+    public double getGasInTank()
+    {
+        return gasInTank;
+    }
+}
