7b116955d012cba4b5f38940afc0ca54d36d158d
prasannabe2004 <prasannabe2004@gmail.com>
2014-09-12 18:17:09 -0500

    Adding edx and java sources.

---
+++ edx/Comp102x/week_1_hello/hello/HelloWorld.java
@@ -0,0 +1,12 @@
+
+/**
+ * A simple program to send a greeting to the world
+ * 
+ */
+public class HelloWorld
+{
+   public static void main(String[] args) {
+       
+       System.out.println("Hello, world!");
+   }
+}
