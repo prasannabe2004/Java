f54d3729ec3319079f02363bae872368f484577a
prasannabe2004 <prasannabe2004@gmail.com>
2015-05-25 17:45:13 -0500

    Solo learn Java examples goes here.

---
+++ sololearn/bucky/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        /*
+        *
+        * This is simple multi line comments.
+        *
+        */
+        System.out.println("Hello World!!");
+        /**
+         * This is java style comments.
+         */
+    }
+}
