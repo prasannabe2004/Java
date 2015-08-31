410125a592001c69616e706313e339e61f791173
prasannabe2004 <prasannabe2004@gmail.com>
2015-08-31 15:46:16 -0500

    First project commit from eclipse.

---
+++ sololearn/FirstProject/src/FirstProject.java
@@ -0,0 +1,12 @@
+
+public class FirstProject {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First Project with git\n");
+	}
+
+}
