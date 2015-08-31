d5f5f59e42cdd1037f39c25ed84e047ed4e83f03
prasannabe2004 <prasannabe2004@gmail.com>
2015-08-31 16:26:49 -0500

    Class and objects explained.

---
+++ sololearn/ClassAndObjects/src/apple.java
@@ -0,0 +1,17 @@
+
+public class apple {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is apple class\n");
+		tuna tunaObject = new tuna();
+		tunaObject.setx(25);
+		System.out.println("Value of x= " + tunaObject.getx());
+		
+		tunaObject.message();
+	}
+
+}
---
+++ sololearn/ClassAndObjects/src/tuna.java
@@ -0,0 +1,17 @@
+
+class tuna {
+	private int x;
+	
+	public int getx() {
+		return x;
+	}
+	
+	public void setx (int y) {
+		this.x= y;
+	}
+	
+	public void message() {
+		System.out.println("This is tuna class\n");
+	}
+
+}
