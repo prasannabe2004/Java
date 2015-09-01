26f13d483b834ff0b801ff9290a8ed0219e0cf07
prasannabe2004 <prasannabe2004@gmail.com>
2015-09-01 16:30:57 -0500

    Constructor example place holder.

---
+++ sololearn/Constructor/src/Constructor.java
@@ -0,0 +1,14 @@
+
+public class Constructor {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello constructor\n");
+
+	}
+
+}
