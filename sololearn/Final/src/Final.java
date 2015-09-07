81a19d421ff8ee7018748015265ec09a5fa32550
prasannabe2004 <prasannabe2004@gmail.com>
2015-09-07 08:51:04 -0500

    Final Keyword shall be initialized in constructor but not anywhere else.

---
+++ sololearn/Final/src/Final.java
@@ -0,0 +1,16 @@
+
+public class Final {
+
+	/**
+	 * @param args
+	 */
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		subclass sub = new subclass();
+		
+		System.out.println(sub.NUMBER);
+		
+	}
+
+}
---
+++ sololearn/Final/src/subclass.java
@@ -0,0 +1,9 @@
+
+public class subclass {
+
+	final int NUMBER ;
+	
+	subclass() {
+		NUMBER = 100;
+	}
+}
