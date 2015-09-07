09f3a6d3f9a4bfa6edc852d3d7da40d44062b2f8
prasannabe2004 <prasannabe2004@gmail.com>
2015-09-06 22:56:29 -0500

    Math examples

---
+++ sololearn/Math/src/Maths.java
@@ -0,0 +1,18 @@
+
+public class Maths {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Math.abs(-24.5));
+		System.out.println(Math.ceil(24.4));
+		System.out.println(Math.floor(24.3));
+		System.out.println(Math.min(24.5,23.5));
+		System.out.println(Math.max(-24.5,-34.5));
+		System.out.println(Math.pow(5,3));
+		System.out.println(Math.sqrt(16));
+	}
+
+}
